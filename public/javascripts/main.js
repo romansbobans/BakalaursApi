0780ccd28e805fc3636a5998ca45bc792f8f2545
Artis Jansons <artis.jansons@testdevlab.com>
2014-06-02 11:44:07 +0300

    JS functionality for objects.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,19 +13,92 @@ $(document).ready(function() {
         this.lang = lang;
         this.name = name;
         this.shortDescription = shortDescription;
+        this.description = null;
+        this.workingHours = null;
+        this.groups = null;
+    }
+
+    function Day(day, from, to) {
+        this.day = day;
+        this.hours = from + " - " + to;
+    }
+
+    function Group(name, price) {
+        this.groupName = name;
+        this.groupPrice = price;
     }
 
     /**
      * Triggers when add category pressed
      */
-    $("#add-category").click(function() {
+    $("#add").click(function() {
         var clonable = $(".panel-wrapper").first().clone(true, true);
         clonable.find("input").val("");
+        clonable.find(("select"))[0].selectedIndex = 0;
         $("#panels").append(clonable.fadeIn("fast"));
     });
 
+    $("#add-day").click(function() {
+        var clonable = $(".days-object").first().clone(true, true);
+        clonable.find("input").val("");
+        clonable.find("#add-day").remove();
+        $(this).closest(".days-wrapper").append(clonable.fadeIn("fast"));
+    });
+
+    $("#add-group").click(function(){
+        var clonable = $(".groups-wrapper .group").first().clone(true, true);
+        console.log(clonable);
+        clonable.find("input").val("");
+        clonable.find("#add-group").remove();
+        $(this).closest(".groups-wrapper").append(clonable.fadeIn("fast"));
+    });
+
+    $("#submit-objects").click(function() {
+
+        var workingHours = [];
+        var groups = [];
+
+        $(".panel-wrapper").each(function() {
+
+            var lang = $(this).find(".lang").val();
+            var name = $(this).find(".name").val();
+            var shortDescription = $(this).find(".shortDescription").val();
+            var description = $(this).find(".description").val();
+
+            var panel = new Panel(lang, name, shortDescription);
+            panel.description = description;
+
+            $(this).find(".days-object").each(function() {
+                var day = $(this).find(".day").val();
+                var from = $(this).find(".from").val();
+                var to = $(this).find(".to").val();
+
+                workingHours.push(new Day(day, from, to));
+            });
+
+            $(this).find(".group").each(function() {
+               var name = $(this).find(".group-name").val();
+               var price = $(this).find(".group-price").val();
+
+               groups.push(new Group(name, price));
+            });
+
+            panel.workingHours = workingHours;
+            panel.groups = groups;
+
+            panels.push(panel);
+        });
+
+        var jsonString = JSON.stringify(panels);
+
+        console.log(jsonString);
+
+        $("#panels-json").val(jsonString);
+        $("#objects-form").submit();
+    });
+
     /**
-     * Triggers when submit button pressed
+     * Triggers when submit for categories button pressed
      */
     $("#submit").click(function() {
 
